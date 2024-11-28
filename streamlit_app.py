b07364f703e26c7a0b890cf504c4362279366e48
pbellgitup <53939046+pbellgitup@users.noreply.github.com>
2024-11-28 10:39:01 -0600

    Update streamlit_app.py
    Updated script to migrate connection from SiS to SniS.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session #Commented out for migration from SiS to SniS.
 from snowflake.snowpark.functions import col
 
 
@@ -15,8 +15,11 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be: ", name_on_order)
 
+#session = get_active_session() #Commented out for migration from SiS to SniS.
 
-session = get_active_session()
+cnx = st.connectino("snowflake")
+session = cns.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
