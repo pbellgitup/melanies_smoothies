ed30dc326d2ec724f4e197f0aa9bbec089bee362
pbellgitup <53939046+pbellgitup@users.noreply.github.com>
2024-11-28 11:26:28 -0600

    Update streamlit_app.py
    Rearranged import requests and moved json to "For Fruit_chosen"

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import requests
+
 #from snowflake.snowpark.context import get_active_session #Commented out for migration from SiS to SniS.
 from snowflake.snowpark.functions import col
 
@@ -35,6 +37,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     #st.write(ingredients_string)
 
@@ -51,10 +55,6 @@ if ingredients_list:
         
         st.success("Your Smoothie is ordered!", icon="✅")
 
-# New Section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 
